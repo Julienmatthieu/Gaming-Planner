5a2e2d91e604faa42a19147538f471f4fb6b981a
Julienmatthieu <julien@bogiraud.com>
2021-06-15 12:45:47 +0100

    debugging event repo

--- connector/event/event_repository.py
+++ connector/event/event_repository.py
@@ -4,22 +4,24 @@ from mysql.connector import Error
 from event import *
 import sys
 sys.path.append('../')
-from connector import *
+from connector
+
+print(globals())
 
 def get_event(event_id):
-    connection = get_connection()
+    connection = connector.get_connection()
     cursor = connection.cursor()
     select_query = """select * from event where id = %s"""
     cursor.execute(select_query, (event_id,))
     records = cursor.fetchall()
     row = records[0]
     current = make_event(row[0], row[1], row[2]) 
-    close_connection(connection)
+    connector.close_connection(connection)
     return current
 
 def get_all_event():
     list = []
-    connection = get_connection()
+    connection = connector.get_connection()
     cursor = connection.cursor()
     select_query = """select * from event"""
     cursor.execute(select_query)
@@ -27,15 +29,15 @@ def get_all_event():
     for row in records:
         current = make_event(row[0], row[1], row[2]) 
         list.append(current)
-    close_connection(connection)
+    connector.close_connection(connection)
     return list
 
 def get_event_from_location(serverId, channelId, messageId):
-    connection = get_connection()
+    connection = connector.get_connection()
     cursor = connection.cursor()
     select_query = f"""select eventId from discordLocation where serverId = {serverId} AND channelId = {channelId} AND messageId = {messageId} """
     cursor.execute(select_query)
     records = cursor.fetchall()
     event = get_event(records[0][0])
-    close_connection(connection)
+    connector.close_connection(connection)
     return event
