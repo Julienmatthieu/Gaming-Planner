49980b96aabb831117d37aaf95bf666b7e66d1d4
Julienmatthieu <julien@bogiraud.com>
2021-06-17 08:53:19 +0100

    author222

--- User/user_service.py
+++ User/user_service.py
@@ -3,13 +3,13 @@ import discord
 import user_repository as repo
 from user import User
 
-async def get_or_create_user(author):
+async def get_or_create_user(author22222):
     print("\n\n --------------- 1 - 1 - 1 ---------------\n\n")
     
-    userDb = await repo.get_user_discordId(author.id)
+    userDb = await repo.get_user_discordId(author22222.id)
     print(f"\n\n --------------- 1 - 1 - 2--------------- {userDb.name}\n\n ")
     if userDb == None:
-        user = User(0, author.name, author.id, author.avatar_url, author.display_name, author.mention)
+        user = User(0, author22222.name, author22222.id, author22222.avatar_url, author22222.display_name, author22222.mention)
         print("\n\n --------------- 1 - 1  - 3 - 1 ---------------\n\n")
         return await repo.create_user(user)
     else: 
