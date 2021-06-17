3c297bacbb25cdfbafa1bc3d45ddaa42b6e1995f
Julienmatthieu <julien@bogiraud.com>
2021-06-17 12:44:46 +0100

    import again

--- Planning/planning_service.py
+++ Planning/planning_service.py
@@ -2,60 +2,60 @@ from os import error
 from sys import audit
 import discord 
 
-import user_service as usr_serv
+from user_service import get_or_create_user
 from event_service import new_event, update_event, get_location_by_event
 import message_service as msg_serv
-from resource import steps, msg_dict, error
+import resource as res
 from Event.event import Location
 from GamePLanner import client 
 
 async def building_together(message):
-    authorDb = await usr_serv.get_or_create_user(message.author)
+    authorDb = await get_or_create_user(message.author)
     event = event_serv.get_last_event_by_userId(authorDb.id)
     authorDb.print()
     event.print()
     print(f" this is the event i found from {authorDb.name} - {event.print()}")
     if event == None:
-        await message.author.send(error['event-not-found'])
+        await message.author.send(res.error['event-not-found'])
         return
     next_step(message, authorDb, event)
 
 #Steps
 async def next_step(message, authorDb, event):
 
-    if event.step == steps['none']:
+    if event.step == res.steps['none']:
         new_event(message, authorDb)
-        event.step = steps['game_name']
+        event.step = res.steps['game_name']
         await update_event(event)
-        await message.author.send(msg_dict['game_name'])
-    elif event.step == steps['init']:
-        event.step = steps['game_name']
+        await message.author.send(res.msg_dict['game_name'])
+    elif event.step == res.steps['init']:
+        event.step = res.steps['game_name']
         await update_event(event)
-        message.author.send(msg_dict['game_name'])
+        message.author.send(res.msg_dict['game_name'])
 
-    elif event.step == steps['game_name']:
+    elif event.step == res.steps['game_name']:
         event.gameName = message.content
-        event.step = steps['slots']
+        event.step = res.steps['slots']
         await update_event(event)
-        message.author.send(msg_dict['slots'])
+        message.author.send(res.msg_dict['slots'])
 
-    elif event.step == steps['slots']:
+    elif event.step == res.steps['slots']:
         event.gameName = message.content
-        event.step = steps['time']
+        event.step = res.steps['time']
         await update_event(event)
-        message.author.send(msg_dict['time'])
+        message.author.send(res.msg_dict['time'])
 
-    elif event.step == steps['time']:
+    elif event.step == res.steps['time']:
         event.gameName = message.content
-        event.step = steps['role']
+        event.step = res.steps['role']
         await update_event(event)
-        message.author.send(msg_dict['role'])
+        message.author.send(res.msg_dict['role'])
 
-    elif event.step == steps['role']:
+    elif event.step == res.steps['role']:
         event.gameName = message.content
-        event.step = steps['done']
+        event.step = res.steps['done']
         await update_event(event)
-        message.author.send(msg_dict['done'])
+        message.author.send(res.msg_dict['done'])
         Location = get_location_by_event(event)
         channel = client.get_channel(int(Location.channelId))
         channel.send(await msg_serv.BuildInvitMessage(event, authorDb))
