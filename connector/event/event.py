83fe0c2d8424b911d9569f7561fd845c891aa9cc
Julienmatthieu <julien@bogiraud.com>
2021-06-17 03:42:14 +0100

    localisation class

--- connector/event/event.py
+++ connector/event/event.py
@@ -7,7 +7,22 @@ class Event(object):
         self.id = id
         self.players = player
         self.time = time
+   
+def build_event(id, players, time):
+    return Event(id, players, time)
 
-def make_event(id, players, time):
-    event = Event(id, players, time)
-    return event
+class Location(object):
+    id = 0
+    serverId = 0
+    channelId = 0
+    messageId = 0
+    eventId = 0
+    def __init__(self, id, serverId, channelId, messageId, eventId):
+        self.id = id
+        self.serverId = serverId
+        self.channelId = channelId
+        self.messageId = messageId
+        self.eventId = eventId
+
+def build_location(id, serverId, channelId, messageId, eventId):
+    return Location(id, serverId, channelId, messageId, eventId)
