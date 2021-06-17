15a3e3f3c1a5a227162362f3cba0fab8f9322138
Julienmatthieu <julien@bogiraud.com>
2021-06-17 07:11:11 +0100

    missing a file

--- User/user.py
+++ User/user.py
@@ -12,7 +12,18 @@ class User(object):
 
     def __init__(self, id, name, discordId, avaterUrl, displayName, mention):
         self.id = id
+        self.name = name
         self.discordId = discordId
         self.avaterUrl = avaterUrl
         self.displayName = displayName
         self.mention = mention
+
+        #testing 
+    def print(self):
+        print(f"this is user {self.id}: \n\
+                            \t name = {self.name} \n\
+                            \t discordId = {self.discordId} \n\
+                            \t avaterUrl = {self.avaterUrl} \n\
+                            \t displayName = {self.displayName} \n\
+                            \t mention = {self.mention} \n\
+                            \n")
