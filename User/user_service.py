feebf62403245c627349003f3aa379b92474c1f5
Julienmatthieu <julien@bogiraud.com>
2021-06-17 07:51:24 +0100

    await

--- User/user_service.py
+++ User/user_service.py
@@ -4,7 +4,7 @@ import user_repository as repo
 from user import User
 
 async def get_or_create_user(author):
-    userDb = repo.get_user_discordId(author.id)
+    userDb = await repo.get_user_discordId(author.id)
     if userDb == None:
         user = User(0, author.name, author.id, author.avatar_url, author.display_name, author.mention)
         return await repo.create_user(user)
