ebde1493c7f05df8459bab514d38cf3838c13e4b
Julienmatthieu <julien@bogiraud.com>
2021-06-17 07:54:51 +0100

    ressources needed in event serv

--- connector/event/event_service.py
+++ connector/event/event_service.py
@@ -2,6 +2,7 @@ import discord
 
 import event_repository as repo
 from event import Event, Location
+import ressources as res
 
 async def get_last_unset_event(message):
     location = await repo.get_last_location(message.guild.id, message.channel.id)
