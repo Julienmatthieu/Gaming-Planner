2fe1cb6430b99c0924f21bf05b747b572d1987a4
Julienmatthieu <julien@bogiraud.com>
2021-06-17 12:38:21 +0100

    fixing circular import

--- Planning/planning_service.py
+++ Planning/planning_service.py
@@ -2,9 +2,8 @@ from os import error
 from sys import audit
 import discord 
 
-
 import user_service as usr_serv
-import event_service as event_serv
+form event_service import new_event, update_event, get_location_by_event
 import message_service as msg_serv
 import resource as res
 from Event.event import Location
@@ -25,39 +24,39 @@ async def building_together(message):
 async def next_step(message, authorDb, event):
 
     if event.step == res.steps['none']:
-        event_serv.new_event(message, authorDb)
+        new_event(message, authorDb)
         event.step = res.steps['game_name']
-        await event_serv.update_event(event)
+        await update_event(event)
         await message.author.send(res.msg_dict['game_name'])
     elif event.step == res.steps['init']:
         event.step = res.steps['game_name']
-        await event_serv.update_event(event)
+        await update_event(event)
         message.author.send(res.msg_dict['game_name'])
 
     elif event.step == res.steps['game_name']:
         event.gameName = message.content
         event.step = res.steps['slots']
-        await event_serv.update_event(event)
+        await update_event(event)
         message.author.send(res.msg_dict['slots'])
 
     elif event.step == res.steps['slots']:
         event.gameName = message.content
         event.step = res.steps['time']
-        await event_serv.update_event(event)
+        await update_event(event)
         message.author.send(res.msg_dict['time'])
 
     elif event.step == res.steps['time']:
         event.gameName = message.content
         event.step = res.steps['role']
-        await event_serv.update_event(event)
+        await update_event(event)
         message.author.send(res.msg_dict['role'])
 
     elif event.step == res.steps['role']:
         event.gameName = message.content
         event.step = res.steps['done']
-        await event_serv.update_event(event)
+        await update_event(event)
         message.author.send(res.msg_dict['done'])
-        Location = event_serv.get_location_by_event(event)
+        Location = get_location_by_event(event)
         channel = client.get_channel(int(Location.channelId))
         channel.send(await msg_serv.BuildInvitMessage(event, authorDb))
 
