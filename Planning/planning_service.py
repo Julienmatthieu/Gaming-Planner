e8d7659c03979ec3b7c8872287149211289446e5
Julienmatthieu <julien@bogiraud.com>
2021-06-17 23:55:37 +0100

    test url

--- Planning/planning_service.py
+++ Planning/planning_service.py
@@ -58,7 +58,7 @@ async def next_step(message, authorDb, event, bot):
         Location = await get_location_by_event(event)
         channel = bot.get_channel(int(Location.channelId))
         await channel.send(
-            msg_serv.BuildInvitMessage(event, authorDb),
+            msg_serv.BuildInvitMessage(event, authorDb) + " https://cdn.discordapp.com/avatars/225743846934446081/02bb90eb50bd3c1070acf282c0dd0c3c.webp?size=1024",
             components = [
                 Button(disabled=0, label = "I\'m in 💪", style = 3),
                 Button(disabled=0, label = "Cancel ❌", style = 4)
