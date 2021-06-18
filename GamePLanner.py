133da067234e10260b6f29cb90b9f316bc4a6dd5
Julienmatthieu <julien@bogiraud.com>
2021-06-18 15:26:17 +0100

    test message

--- GamePLanner.py
+++ GamePLanner.py
@@ -39,7 +39,11 @@ async def DefaultPlanning(ctx):
     
     authorDb = await usr_serv.get_or_create_user(ctx.message.author)
     event = await event_serv.get_by_id(153)
-    await ctx.message.channel.send(embed=msg_serv.BuildInvitMessage(event, authorDb))
+    await ctx.message.channel.send(
+        type = 1,
+        embed=msg_serv.BuildInvitMessage(event, authorDb),
+        components = []
+    )
 
 
 @bot.command(name='list', help="test a changer")
