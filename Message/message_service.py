5c4bf7601d33994b597458abc797ab775a81bb1d
Julienmatthieu <julien@bogiraud.com>
2021-06-18 13:15:19 +0100

    new description

--- Message/message_service.py
+++ Message/message_service.py
@@ -11,7 +11,7 @@ def BuildInvitMessage(event, author):
     players = event.get_list_players()
 
 
-    embed=discord.Embed(title=f"Let's play {event.slots}", description=f"Hello i wanna play some **{event.gameName}** and I\'m looking for **{event.slots}** people(s) to join me.", color=discord.Color.dark_gold())
+    embed=discord.Embed(title=f"Let's play some {event.gameName}", description=f"Hi I\'m  looking for **{event.slots}** people(s) to join me to play some **{event.gameName}**  to join me.", color=discord.Color.dark_gold())
     embed.set_thumbnail(url="https://compass-ssl.xboxlive.com/assets/1f/35/1f355aca-753c-4213-8a42-563128129070.jpg?n=Parallax_Sections_Large_Desktop_01.jpg")
     embed.set_author(name=author.displayName, icon_url=author.avatarUrl)
     message = ""
@@ -21,7 +21,7 @@ def BuildInvitMessage(event, author):
         else:
             message += f'\n\t  **➤** '
 
-    embed.add_field(name="Team:", value=message, inline=True)
+    embed.add_field(name="Team:", value=message, inline=False)
 
 #    embed.add_field(name=f"**{author.mention}** lance une session de  **{event.gameName}** ", value=f"**Heure:** **`{event.time}`\n**➤ Places:**{str(event.slots - len(players))}** \n", inline=True)
 #    embed.add_field(name=f"\n Participant: ", value=message, inline=False)
