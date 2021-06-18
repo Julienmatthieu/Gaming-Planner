7ae9e6280592fa3a5e32ea0355b67f846143b2a5
Julienmatthieu <julien@bogiraud.com>
2021-06-18 17:29:01 +0100

    await message

--- GamePLanner.py
+++ GamePLanner.py
@@ -50,11 +50,11 @@ async def DefaultPlanning(ctx):
         ]
     )
     interaction = await bot.wait_for("button_click", check = lambda i: i.component.label.startswith("I"))
-    interaction.channel.send("message bien reçu")
+    await interaction.channel.send(f"TEST Cliccked by {interaction.author.name} on {interaction.component.label} name  ")
+
 #    while test == True:
 #        if interaction.component.label == "I\'m in":
 #            await interaction.respond(content=f"TEST {interaction.author.mention} you have been added to the team  ")
-#            await interaction.channel.send(f"TEST Cliccked by {interaction.author.name} on {interaction.component.label} name  ")
 #            await interaction.message.edit(
 #                type = 1,
 #                embed=msg_serv.BuildInvitMessage(event, authorDb),
