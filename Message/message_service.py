44389fafcc2110ebb5bfb88f1f83f573779ddc9f
Julienmatthieu <julien@bogiraud.com>
2021-06-18 12:41:55 +0100

    change message builder

--- Message/message_service.py
+++ Message/message_service.py
@@ -1,22 +1,24 @@
 from event import Event
 from user import User
 from resources import msg_type 
+import discord
 
 # Event setting
 def BuildInvitMessage(event, author):
+    embed = discord.Embed()
     if event == None:
         return ''
     players = event.get_list_players()
 
-    message= f'@{event.role} '         
-    message += f'\n>>> \n\t**{author.displayName}** lance une session de  **{event.gameName}**  <@!225743846934446081> '
-    message += f'\n\t\theure:  **{event.time}** \t\t **{str(event.slots - len(players))}** place(s)\n'
+    message = ""
     for slot in range(event.slots):
         if slot < len(players):
-            message += f'\n\t  - {players[slot]}'
+            message += f'\n\t  **➤ {players[slot]}**'
         else:
-            message += f'\n\t  - '
-    message += '\n\n   '
+            message += f'\n\t  **➤** '
+    embed.add_field(name=f"**{author.mention}** lance une session de  **{event.gameName}** ", value=f"**Heure:** **`{event.time}`\n**➤ Places:**{str(event.slots - len(players))}** \n", inline=True)
+    embed.add_field(name=f"\n Participant: ", value=message, inline=False)
+
     return message
 
 async def FullClear(message):
@@ -24,3 +26,12 @@ async def FullClear(message):
         return
     async for message in message.channel.history(limit=None):
         await message.delete()
+
+
+       
+
+        list = ""
+
+
+        embed.add_field(name="Senna", value="**➤ Name:** `Senna`\n**➤ Level:** 5\n**➤ Points:** 24526", inline=True)
+        await ctx.send(embed=embed)
