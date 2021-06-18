4b112781f5cae2a0e1f2b7af13147b625a2be73d
Julienmatthieu <julien@bogiraud.com>
2021-06-18 15:44:35 +0100

    repond

--- GamePLanner.py
+++ GamePLanner.py
@@ -56,7 +56,7 @@ async def DefaultPlanning(ctx):
             test = False
         else:
             await interaction.channel.send(f"Cliccked by {interaction.author.name}")
-        await interaction.respond(f"Cliccked by {interaction.author.name} on {interaction.component.label} name  ")
+        await interaction.respond("Cliccked by name  ")
 
 @bot.command(name='list', help="test a changer")
 async def button(ctx):
