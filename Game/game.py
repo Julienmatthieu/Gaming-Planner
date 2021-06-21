a42e5119f23528b649e5998f23a13d612e72832e
Julienmatthieu <julien@bogiraud.com>
2021-06-21 11:26:57 +0100

    adding game gestion

---
+++ Game/game.py
@@ -0,0 +1,9 @@
+class Game(object):
+    id = 0
+    name = ""
+    image = ""
+
+    def __init__(self, id, name, image):
+        self.id = id
+        self.name = name
+        self.image = image
---
+++ Game/game_repository.py
@@ -0,0 +1,23 @@
+from game import Game 
+import connector 
+
+async def create_game(game):
+    query = f"""INSERT INTO game (name, image) VALUES (\"{game.name}\",\"{game.image}\")"""
+    game.id = connector.alter_query(query)
+    return game
+
+async def get_game_by_name(name):
+    records = connector.select_query(f"""SELECT * FROM game WHERE name = {name}""")
+    if len(records) == 0:
+        return None
+    row = records[0]
+    game = Game(row[0], row[1], row[2]) 
+    return game 
+
+async def get_game(id):
+    records = connector.select_query(f"""SELECT * FROM game WHERE id = {id}""")
+    if len(records) == 0:
+        return None
+    row = records[0]
+    game = Game(row[0], row[1], row[2]) 
+    return game 
