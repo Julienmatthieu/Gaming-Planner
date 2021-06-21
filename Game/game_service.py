f98a40670587c2e19b368d550bcdb84f6aa20821
Julienmatthieu <julien@bogiraud.com>
2021-06-21 13:22:36 +0100

    game name lower + capitalize

--- Game/game_service.py
+++ Game/game_service.py
@@ -2,6 +2,7 @@ import game_repository as repo
 from game import Game
 
 async def known_game(name):
+    name=name.lower().capitalize()
     game_db = await repo.get_game_by_name(name)
     return game_db
 
@@ -9,5 +10,4 @@ async def get_game(id):
     return await repo.get_game(id)
 
 async def add_image(game_id, image_url):
-    print("    ------------- adding image ------------------")
     return await repo.update_image_by_id(game_id, image_url)
