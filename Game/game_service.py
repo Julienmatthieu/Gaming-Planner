87e949b66f6551f47f4e4d486db7d1ca00b81ddc
Julienmatthieu <julien@bogiraud.com>
2021-06-21 12:38:25 +0100

    one more await

--- Game/game_service.py
+++ Game/game_service.py
@@ -2,7 +2,7 @@ import game_repository as repo
 from game import Game
 
 async def known_game(name):
-    game_db = repo.get_game_by_name(name)
+    game_db = await repo.get_game_by_name(name)
     if game_db.image == "":
         return False
     else:
