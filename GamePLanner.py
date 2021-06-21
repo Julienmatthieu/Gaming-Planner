0a275c84251ba8eb0f29e86ab9191f8d9f320b17
Julienmatthieu <julien@bogiraud.com>
2021-06-21 12:29:20 +0100

    no duble command

--- GamePLanner.py
+++ GamePLanner.py
@@ -69,7 +69,6 @@ async def on_message(message):
     elif str(message.channel.type) == res.msg_type['dm'] and message.author != bot.user:
         await plan_serv.building_together(message, bot)
 
-@bot.command()
 @bot.command(name=res.next, help=res.help['next'])
 async def next(ctx):
     message = ctx.message
