660c89e5e87e527477f086d8b93b2a94041c018e
Julienmatthieu <julien@bogiraud.com>
2021-06-21 13:17:32 +0100

    get_game

--- Game/game_service.py
+++ Game/game_service.py
@@ -4,7 +4,10 @@ from game import Game
 async def known_game(name):
     game_db = await repo.get_game_by_name(name)
     return game_db
-    
+
+async def get_game(id):
+    return await repo.get_game(id)
+
 async def add_image(game_id, image_url):
     print("    ------------- adding image ------------------")
     return await repo.update_image_by_id(game_id, image_url)
