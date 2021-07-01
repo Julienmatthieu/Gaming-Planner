73c55a6d2bb8032948ab00bb41a0e4035290a1c0
Julienmatthieu <julien@bogiraud.com>
2021-07-01 14:08:07 +0100

    deleting flooding orint

--- User/user_service.py
+++ User/user_service.py
@@ -6,18 +6,11 @@ from user import User
 async def get_or_create_user(author_discord):
     author = User(0, author_discord.display_name, author_discord.id, author_discord.avatar_url, author_discord.display_name, author_discord.mention)
     userDb = await repo.get_user_discordId(author.discord_id)
-    print("------------------------ 1 ")
     if userDb == None:
-        print("------------------------ 2.1 ")
         return await repo.create_user(author)
     elif userDb.need_update(author):
-        print("------------------------ 2.2.1 ---- Copy")
-        userDb.print()
         userDb.copy(author)
-        userDb.print()
-        print("------------------------ 2.2.2 ")
         await repo.update_user(userDb)
-        print("------------------------ 2.2.3 ")
     return userDb
 
 async def get_by_discord_id(discird_id):
