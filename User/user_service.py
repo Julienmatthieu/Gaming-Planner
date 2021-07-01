a554ca99c8874f451f6c2abda703ca49e1d841f7
Julienmatthieu <julien@bogiraud.com>
2021-07-01 13:24:04 +0100

    adding test ^print on copy user

--- User/user_service.py
+++ User/user_service.py
@@ -6,13 +6,20 @@ from user import User
 async def get_or_create_user(author_discord):
     author = User(0, author_discord.display_name, author_discord.id, author_discord.avatar_url, author_discord.display_name, author_discord.mention)
     userDb = await repo.get_user_discordId(author.id)
+    print("-----------------------")
+    author.print()
+    userDb.print()
+    print("-----------------------")
     if userDb == None:
         return await repo.create_user(author)
-    else: 
-        if userDb.need_update(userDb.need_update(author)):
-            await repo.update_user(userDb)
-            userDb.copy(author)
-        return userDb
+    elif userDb.need_update(userDb.need_update(author)):
+        userDb.copy(author)
+        await repo.update_user(userDb)
+        print("-----------------------")
+        author.print()
+        userDb.print()
+        print("-----------------------")
+    return userDb
 
 async def get_by_discord_id(discird_id):
     return await repo.get_user_discordId(discird_id)
