8687defe2a0e552fb1a5e6693cb8e4cb2999eb09
Julienmatthieu <julien@bogiraud.com>
2021-07-01 14:28:40 +0100

    convert discord addt to str

--- User/user_service.py
+++ User/user_service.py
@@ -4,7 +4,7 @@ import user_repository as repo
 from user import User
 
 async def get_or_create_user(author_discord):
-    author = User(0, author_discord.display_name, author_discord.id, author_discord.avatar_url.str(), author_discord.display_name, author_discord.mention)
+    author = User(0, author_discord.display_name, author_discord.id, str(author_discord.avatar_url), author_discord.display_name, author_discord.mention)
     userDb = await repo.get_user_discordId(author.discord_id)
     if userDb == None:
         return await repo.create_user(author)
