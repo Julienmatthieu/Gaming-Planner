b98392d03f2e0394261da8279b67267c3ac83b0e
Julienmatthieu <julien@bogiraud.com>
2021-07-05 16:26:28 +0100

    deleting print test

--- Event/event.py
+++ Event/event.py
@@ -48,7 +48,6 @@ class Event(object):
         self.game_id = other.game_id
 
     def add_player(self, user):
-        user.print()
         if user.displayName in self.players:
             return
         self.players_id.append(user.discord_id)
